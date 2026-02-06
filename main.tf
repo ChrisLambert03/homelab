5f0b4c7c215e69c58e265f96a9fd7ab38cbacfb3
Chris Lambert <chrislambert352@gmail.com>
2026-02-06 04:45:36 -0500

    jellyfin setup

--- main.tf
+++ main.tf
@@ -3,3 +3,47 @@ resource "docker_image" "jellyfin" {
   name         = "jellyfin/jellyfin:latest"
   keep_locally = true # Don't delete the image after creating the container (on destroy)
 }
+# Create a Docker network for the homelab containers
+resource "docker_network" "homelab_network" {
+  name   = "homelab"
+  driver = "bridge"
+}
+# Jellyfin docker container definition
+resource "docker_container" "jellyfin" {
+  name  = "jellyfin"
+  image = docker_image.jellyfin.image_id
+  restart = "unless-stopped"
+
+  ports {
+    internal = 8096
+    external = 8096
+  }
+  # Connect the container to the homelab network
+  networks_advanced {
+    name = docker_network.homelab_network.name
+  }
+  env = [
+    "PUID=1000",
+    "PGID=1000",
+    "TZ=America/New_York",
+    "JELLYFIN_PublishedServerUrl=http://${var.workstation_ip}:8096"
+  ]
+
+  # Config folder for metadata/database
+  mounts {
+    target = "/config"
+    source = var.jellyfin_config_path
+    type   = "bind"
+  }
+  # All media mapped to /data
+  mounts {
+    target = "/data/blue_drive"
+    source = var.blue_drive_path
+    type   = "bind"
+  }
+  mounts{
+   target = "/data/black_drive"
+   source = var.black_drive_path
+   type   = "bind"
+  }
+}
