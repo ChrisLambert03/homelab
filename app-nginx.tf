aceee23bdabba8fb025020fbd8b5082a59d9f22e
Chris Lambert <chrislambert352@gmail.com>
2026-02-06 23:16:08 -0500

    added prowlarr

--- app-nginx.tf
+++ app-nginx.tf
@@ -125,3 +125,22 @@ resource "nginxproxymanager_proxy_host" "radarr_proxy" {
   hsts_subdomains = true
   http2_support   = true 
 }
+
+resource "nginxproxymanager_proxy_host" "prowlarr_proxy" {
+  domain_names = ["prowlarr.${var.homlab_domain}"]
+
+  forward_scheme = "http"
+  forward_host   = var.workstation_ip
+  forward_port   = 9696
+
+  caching_enabled         = true
+  allow_websocket_upgrade = true
+  block_exploits          = true
+
+  certificate_id  = var.wildcard_cert_id
+  ssl_forced      = true
+  hsts_enabled    = true
+  hsts_subdomains = true
+  http2_support   = true
+  
+}
---
+++ app-prowlarr.tf
@@ -0,0 +1,24 @@
+# 1. Pull the Prowlarr Image
+resource "docker_image" "prowlarr" {
+  name = "lscr.io/linuxserver/prowlarr:latest"
+}
+
+# 2. Prowlarr Container
+resource "docker_container" "prowlarr" {
+  name         = "prowlarr"
+  image        = docker_image.prowlarr.image_id
+  restart      = "unless-stopped"
+  network_mode = "host"
+
+  env = [
+    "PUID=1000",
+    "PGID=1000",
+    "TZ=America/New_York"
+  ]
+
+  mounts {
+    target = "/config"
+    source = "/home/chris/services/prowlarr/config"
+    type   = "bind"
+  }
+}
