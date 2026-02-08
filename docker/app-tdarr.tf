1af8478045325ba6f27dd252c55223624865c2b0
Chris Lambert <chrislambert352@gmail.com>
2026-02-08 09:10:11 -0500

    added tdarr node

--- docker/app-tdarr.tf
+++ docker/app-tdarr.tf
@@ -18,7 +18,12 @@ resource "docker_container" "tdarr_server" {
     "serverIP=100.106.96.18", #Thinkmate Node IP
     "serverPort=8266",
     "webUIPort=8265",
-    "internalNode=false" # Disabled 
+    "internalNode=true",
+    "inContainer=true", # Disabled 
+    "ffmpegVersion=7",
+    "nodeName=ThinkmateServer",
+    "openBrowser=true",
+    "maxLogSizeMB=10"
   ]
 
   mounts {
@@ -48,5 +53,67 @@ resource "docker_container" "tdarr_server" {
     target = "/more_media"
     source = "/mnt/blue_drive/data" # Your second HDD library
     type   = "bind"
+  }
+}
+
+
+resource "docker_image" "tdarr_node" {
+  name = "ghcr.io/haveagitgat/tdarr_node:latest"
+}
+
+resource "docker_container" "tdarr_node" {
+  name         = "tdarr-node"
+  image        = docker_image.tdarr_node.image_id
+  restart      = "unless-stopped"
+  
+  # Changed to host mode
+  network_mode = "host"
+
+  env = [
+    "TZ=America/New_York",
+    "PUID=1000",
+    "PGID=1000",
+    "UMASK_SET=002",
+    "nodeName=ThinkmateNode",
+    "serverIP=100.106.96.18", # Since it's on the same host, 0.0.0.0 or localhost works
+    "serverPort=8266",
+    "inContainer=true",
+    "ffmpegVersion=7",
+    "nodeType=mapped",
+    "priority=-1",
+    "maxLogSizeMB=10",
+    "pollInterval=2000",
+    "transcodegpuWorkers=0",
+    "transcodecpuWorkers=2"
+  ]
+
+  # Mounts - Keeping your paths consistent
+  mounts {
+    target = "/app/configs"
+    source = "/home/chris/services/tdarr/node/configs"
+    type   = "bind"
+  }
+
+  mounts {
+    target = "/app/logs"
+    source = "/home/chris/services/tdarr/node/logs"
+    type   = "bind"
+  }
+
+  mounts {
+    target = "/temp"
+    source = "/mnt/transcode_cache"
+    type   = "bind"
+  }
+
+  mounts {
+    target = "/media"
+    source = "/mnt/black_drive/data"
+    type   = "bind"
+  }
+  mounts {
+        target = "/more_media"
+        source = "/mnt/blue_drive/data"
+        type   = "bind"
   }
 }
