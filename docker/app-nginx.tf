ba7e7738ccc723f442cdc73e5147a9389b6724d9
Chris Lambert <chrislambert352@gmail.com>
2026-02-08 15:12:15 -0500

    added nginx proxy host

--- docker/app-nginx.tf
+++ docker/app-nginx.tf
@@ -163,3 +163,22 @@ resource "nginxproxymanager_proxy_host" "tdarr_proxy" {
   http2_support   = true
   
 }
+
+resource "nginxproxymanager_proxy_host" "nginx_proxy" {
+  domain_names = ["nginx.${var.homlab_domain}"]
+
+  forward_scheme = "http"
+  forward_host   = var.workstation_ip
+  forward_port   = 81
+
+  caching_enabled         = true
+  allow_websocket_upgrade = true
+  block_exploits          = true
+
+  certificate_id  = var.wildcard_cert_id
+  ssl_forced      = true
+  hsts_enabled    = true
+  hsts_subdomains = true
+  http2_support   = true
+  
+}
