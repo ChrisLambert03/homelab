7e39cdb96db38f22a6dd6e3ebfebe9facae48106
Chris Lambert <chrislambert352@gmail.com>
2026-02-15 17:54:47 -0500

    added grafana proxy

--- docker/app-nginx.tf
+++ docker/app-nginx.tf
@@ -182,3 +182,22 @@ resource "nginxproxymanager_proxy_host" "nginx_proxy" {
   http2_support   = true
   
 }
+
+resource "nginxproxymanager_proxy_host" "grafana_proxy" {
+  domain_names = ["grafana.${var.homlab_domain}"]
+
+  forward_scheme = "http"
+  forward_host   = var.lenovo_thinkcentre_ip
+  forward_port   = 3030
+
+  caching_enabled         = true
+  allow_websocket_upgrade = true
+  block_exploits          = true
+
+  certificate_id  = var.wildcard_cert_id
+  ssl_forced      = true
+  hsts_enabled    = true
+  hsts_subdomains = true
+  http2_support   = true
+  
+}
