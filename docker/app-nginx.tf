4730a3fda98b8fa63b4f86bfbec41449d458b385
Chris Lambert <chrislambert352@gmail.com>
2026-02-26 23:43:29 -0500

    added ntfy proxy

--- docker/app-nginx.tf
+++ docker/app-nginx.tf
@@ -220,3 +220,24 @@ resource "nginxproxymanager_proxy_host" "homarr_proxy" {
   http2_support   = true
   
 }
+
+
+#ntfy proxy
+resource "nginxproxymanager_proxy_host" "ntfy_proxy" {
+  domain_names = ["ntfy.${var.homlab_domain}"]
+
+  forward_scheme = "http"
+  forward_host   = var.lenovo_thinkcentre_ip
+  forward_port   = 2323
+
+  caching_enabled         = true
+  allow_websocket_upgrade = true
+  block_exploits          = true
+
+  certificate_id  = var.wildcard_cert_id
+  ssl_forced      = true
+  hsts_enabled    = true
+  hsts_subdomains = true
+  http2_support   = true
+  
+}
