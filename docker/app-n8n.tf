37ee5a1be4da58b3639057dbe9952c2554d1ec9b
Chris Lambert <chrislambert352@gmail.com>
2026-03-15 11:57:52 -0400

    added n8n

---
+++ docker/app-n8n.tf
@@ -0,0 +1,27 @@
+resource "docker_volume" "n8n_data" {
+  name = "n8n_data"
+}
+
+resource "docker_image" "n8n" {
+  name         = "docker.n8n.io/n8nio/n8n:latest"
+  keep_locally = true
+}
+
+resource "docker_container" "n8n" {
+  name    = "n8n"
+  image   = docker_image.n8n.image_id
+  restart = "unless-stopped"
+  network_mode = "host"
+
+  env = [
+    "N8N_HOST=n8n.${var.homlab_domain}",
+    "WEBHOOK_URL=https://n8n.${var.homlab_domain}",
+    "GENERIC_TIMEZONE=America/New_York",
+    "N8N_ENCRYPTION_KEY=${var.n8n_encryption_key}",
+  ]
+
+  volumes {
+    volume_name    = docker_volume.n8n_data.name
+    container_path = "/home/node/.n8n"
+  }
+}
--- nginx/proxy_hosts.tf
+++ nginx/proxy_hosts.tf
@@ -191,3 +191,19 @@ resource "nginxproxymanager_proxy_host" "cockpit_proxy" {
   hsts_subdomains         = true
   http2_support           = true
 }
+
+resource "nginxproxymanager_proxy_host" "n8n_proxy" {
+  domain_names            = ["n8n.${var.homlab_domain}"]
+  access_list_id          = var.access_list_id
+  forward_scheme          = "http"
+  forward_host            = var.workstation_ip
+  forward_port            = 5678
+  caching_enabled         = true
+  allow_websocket_upgrade = true
+  block_exploits          = true
+  certificate_id          = var.wildcard_cert_id
+  ssl_forced              = true
+  hsts_enabled            = true
+  hsts_subdomains         = true
+  http2_support           = true
+}
