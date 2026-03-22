8c70d64c4e0108f71c7e20cce5ce42e3eec850bf
Chris Lambert <chrislambert352@gmail.com>
2026-03-21 23:25:35 -0400

    lint

--- docker/lenovo/providers.tf
+++ docker/lenovo/providers.tf
@@ -2,8 +2,8 @@ terraform {
   required_version = ">= 1.14.6"
   required_providers {
     docker = {
-      source  = "kreuzwerker/docker"
-      version = "3.6.2"
+      source                = "kreuzwerker/docker"
+      version               = "3.6.2"
       configuration_aliases = [docker.lenovo]
     }
   }
