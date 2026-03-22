2486fc52e1637eb1036459b25a9e845a0b270b7d
Chris Lambert <chrislambert352@gmail.com>
2026-03-21 23:51:30 -0400

    added ntfy

---
+++ docker/lenovo/app-ntfy.tf
@@ -0,0 +1,39 @@
+resource "docker_image" "ntfy" {
+  provider = docker.lenovo
+  name     = "binwiederhier/ntfy:latest"
+}
+
+resource "docker_container" "ntfy" {
+  provider = docker.lenovo
+  name     = "ntfy"
+  image    = docker_image.ntfy.image_id
+  restart  = "unless-stopped"
+  command  = ["serve"]
+
+  env = [
+    "TZ=America/New_York"
+  ]
+
+  ports {
+    internal = 80
+    external = 2323
+  }
+
+  volumes {
+    host_path      = "/home/chris/services/ntfy/var/cache/ntfy"
+    container_path = "/var/cache/ntfy"
+  }
+
+  volumes {
+    host_path      = "/home/chris/services/ntfy/etc/ntfy"
+    container_path = "/etc/ntfy"
+  }
+
+  healthcheck {
+    test         = ["CMD-SHELL", "wget -q --tries=1 http://localhost:80/v1/health -O - | grep -Eo '\"healthy\"\\s*:\\s*true' || exit 1"]
+    interval     = "60s"
+    timeout      = "10s"
+    retries      = 3
+    start_period = "40s"
+  }
+}
