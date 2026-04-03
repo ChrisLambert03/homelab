f06d436b3f7ccf6b66f9c5314ad660f34ecf7ef8
Chris Lambert <chrislambert352@gmail.com>
2026-04-02 20:50:50 -0400

    terraform syntax format

--- providers.tf
+++ providers.tf
@@ -1,23 +1,4 @@
 # Configure the Docker provider to connect to a remote Docker host via SSH
-terraform {
-  required_version = ">= 1.14.6"
-
-  required_providers {
-    docker = {
-      source  = "kreuzwerker/docker"
-      version = "3.6.2"
-    }
-    nginxproxymanager = {
-      source  = "Sander0542/nginxproxymanager"
-      version = "1.2.2"
-    }
-    libvirt = {
-      source  = "dmacvicar/libvirt"
-      version = ">= 0.9.4"
-    }
-  }
-}
-
 provider "docker" {
   host     = "ssh://${var.ssh_user}@${var.workstation_ip}:22"
   ssh_opts = ["-o", "StrictHostKeyChecking=no", "-o", "UserKnownHostsFile=/dev/null"]
