81e5dcb661930419849d392955ededad3067ff8f
Chris Lambert <chrislambert352@gmail.com>
2026-05-08 22:56:37 -0400

    cloud remote backend complete

---
+++ backend.tf
@@ -0,0 +1,10 @@
+terraform {
+  cloud {
+    organization = "hcp-lambertlab-us"
+    hostname = "app.terraform.io"
+    workspaces {
+      project = "homelab-project"
+      name    = "homelab-workspace"
+    }
+  }
+}
