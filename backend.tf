2177d7622e07e02abde33e0f66cac3e00f2be668
Chris Lambert <chrislambert352@gmail.com>
2026-05-08 22:56:56 -0400

    fmt

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   cloud {
     organization = "hcp-lambertlab-us"
-    hostname = "app.terraform.io"
+    hostname     = "app.terraform.io"
     workspaces {
       project = "homelab-project"
       name    = "homelab-workspace"
