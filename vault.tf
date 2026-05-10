ed6a0c5dc22eb89f2345ea9d6e90e5b4cbc8868b
Chris Lambert <chrislambert352@gmail.com>
2026-05-10 15:07:11 -0400

    fmt

--- vault.tf
+++ vault.tf
@@ -15,23 +15,23 @@ locals {
   jellyfin_macvlan_ip   = tostring(data.vault_kv_secret_v2.homelab_secrets.data["jellyfin_macvlan_ip"])
 
   # Authentication
-  ssh_user      = tostring(data.vault_kv_secret_v2.homelab_secrets.data["ssh_user"])
-  npm_user      = tostring(data.vault_kv_secret_v2.homelab_secrets.data["npm_user"])
-  npm_password  = tostring(data.vault_kv_secret_v2.homelab_secrets.data["npm_password"])
+  ssh_user     = tostring(data.vault_kv_secret_v2.homelab_secrets.data["ssh_user"])
+  npm_user     = tostring(data.vault_kv_secret_v2.homelab_secrets.data["npm_user"])
+  npm_password = tostring(data.vault_kv_secret_v2.homelab_secrets.data["npm_password"])
 
   # Encryption & Secret Keys
-  n8n_key             = tostring(data.vault_kv_secret_v2.homelab_secrets.data["n8n_key"])
-  homarr_secret_key   = tostring(data.vault_kv_secret_v2.homelab_secrets.data["homarr_secret_key"])
-  kibana_secret_key   = tostring(data.vault_kv_secret_v2.homelab_secrets.data["kibana_secret_key"])
-  elastic_password    = tostring(data.vault_kv_secret_v2.homelab_secrets.data["elastic_password"])
-  postgres_password   = tostring(data.vault_kv_secret_v2.homelab_secrets.data["postgres_password"])
+  n8n_key           = tostring(data.vault_kv_secret_v2.homelab_secrets.data["n8n_key"])
+  homarr_secret_key = tostring(data.vault_kv_secret_v2.homelab_secrets.data["homarr_secret_key"])
+  kibana_secret_key = tostring(data.vault_kv_secret_v2.homelab_secrets.data["kibana_secret_key"])
+  elastic_password  = tostring(data.vault_kv_secret_v2.homelab_secrets.data["elastic_password"])
+  postgres_password = tostring(data.vault_kv_secret_v2.homelab_secrets.data["postgres_password"])
 
   # Access Control
   access_list_id   = tonumber(data.vault_kv_secret_v2.homelab_secrets.data["access_list_id"])
   access_list_id_2 = tonumber(data.vault_kv_secret_v2.homelab_secrets.data["access_list_id_2"])
 
   # Configuration
-  homlab_domain = tostring(data.vault_kv_secret_v2.homelab_secrets.data["homlab_domain"])
+  homlab_domain    = tostring(data.vault_kv_secret_v2.homelab_secrets.data["homlab_domain"])
   wildcard_cert_id = tonumber(data.vault_kv_secret_v2.homelab_secrets.data["wildcard_cert_id"])
 
   # Paths
