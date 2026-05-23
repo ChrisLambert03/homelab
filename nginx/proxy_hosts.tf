3930f3efb82b6e02422bce8822f66de2fd688dc0
Chris Lambert <chrislambert352@gmail.com>
2026-05-23 10:58:51 -0400

    feat(proxy): add vault to nginx proxy hosts

--- nginx/proxy_hosts.tf
+++ nginx/proxy_hosts.tf
@@ -78,6 +78,22 @@ resource "nginxproxymanager_proxy_host" "portainer_proxy" {
   http2_support           = true
 }
 
+resource "nginxproxymanager_proxy_host" "vault_proxy" {
+  domain_names            = ["vault.${var.homlab_domain}"]
+  access_list_id          = var.access_list_id
+  forward_scheme          = "http"
+  forward_host            = var.optiplex9020_ip
+  forward_port            = 8222
+  caching_enabled         = true
+  allow_websocket_upgrade = true
+  block_exploits          = true
+  certificate_id          = var.wildcard_cert_id
+  ssl_forced              = true
+  hsts_enabled            = true
+  hsts_subdomains         = true
+  http2_support           = true
+}
+
 resource "nginxproxymanager_proxy_host" "sonarr_proxy" {
   domain_names            = ["sonarr.${var.homlab_domain}"]
   access_list_id          = var.access_list_id
