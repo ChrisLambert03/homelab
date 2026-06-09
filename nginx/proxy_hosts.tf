63022c38b04eb3315bb557ef96f881201ae5a600
Chris Lambert <chrislambert352@gmail.com>
2026-06-09 00:19:30 -0400

    chore(nginx): update ntfy forward port for k8s migration

--- nginx/proxy_hosts.tf
+++ nginx/proxy_hosts.tf
@@ -196,7 +196,7 @@ resource "nginxproxymanager_proxy_host" "ntfy_proxy" {
 
   forward_scheme          = "http"
   forward_host            = var.lenovo_thinkcentre_ip
-  forward_port            = 2323
+  forward_port            = 32080
   caching_enabled         = true
   allow_websocket_upgrade = true
   block_exploits          = true
