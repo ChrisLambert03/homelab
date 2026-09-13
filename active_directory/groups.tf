8cede4776c5304b64f9e5a8da3ea660d6a3c70be
Chris Lambert <chrislambert352@gmail.com>
2026-09-13 17:59:40 -0400

    feat(ad): create OPNsense-Admins security group and add user chris

--- active_directory/groups.tf
+++ active_directory/groups.tf
@@ -4,3 +4,20 @@
 
 # Core directory administrative groups (Domain Admins) are managed natively
 # within Active Directory to protect built-in administrative accounts.
+
+resource "ad_group" "opnsense_admins" {
+  name             = "OPNsense-Admins"
+  sam_account_name = "OPNsense-Admins"
+  scope            = "global"
+  category         = "security"
+  container        = ad_ou.groups.dn
+  description      = "OPNsense Firewall Administrators"
+}
+
+resource "ad_group_membership" "opnsense_admins" {
+  group_id = ad_group.opnsense_admins.id
+  group_members = [
+    ad_user.chris.id
+  ]
+}
+
